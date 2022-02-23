5e574c6047d5a814e1a81a020d7b5861967296e3
Steve Bauman <steven_bauman@outlook.com>
2022-02-23 09:59:41 -0500

    Avoid loading if the current directory has spatie ray in it's composer json

--- loader.php
+++ loader.php
@@ -6,6 +6,27 @@ $version = $match[0];
 
 $pharPath = __DIR__ . "/generator/phars/ray_php_{$version}.phar";
 
+$packages = [
+    'spatie/ray',
+    'spatie/yii-ray',
+    'spatie/craft-ray',
+    'spatie/laravel-ray',
+];
+
+$composerJson = getcwd() . '/composer.json';
+
+if (file_exists($composerJson)) {
+    $composer = json_decode(file_get_contents($composerJson), true);
+
+    foreach (['require', 'require-dev'] as $require) {
+        foreach ($composer[$require] ?? [] as $package => $version) {
+            if (in_array($package, $packages)) {
+                return;
+            }
+        }
+    }
+}
+
 if (file_exists($pharPath)) {
     require_once $pharPath;
 }
